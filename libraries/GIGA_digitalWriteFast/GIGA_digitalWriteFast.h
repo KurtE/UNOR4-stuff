3ae93fb7457cdb9613c031eef1c2135ae7a5a564
kurte <kurte@rockisland.com>
2023-10-21 07:06:17 -0700

    Update GIGA_digitalWriteFast.h

--- libraries/GIGA_digitalWriteFast/GIGA_digitalWriteFast.h
+++ libraries/GIGA_digitalWriteFast/GIGA_digitalWriteFast.h
@@ -33,7 +33,7 @@
 #endif
 
 
-static  GPIO_TypeDef * const port_table[] = { GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOE, GPIOF, GPIOG, GPIOH, GPIOI, GPIOJ, GPIOK };
+static  GPIO_TypeDef * const port_table[] = { GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF, GPIOG, GPIOH, GPIOI, GPIOJ, GPIOK };
 static const uint16_t mask_table[] = { 1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7,
                                        1 << 8, 1 << 9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15 };
 
@@ -43,7 +43,7 @@ static inline void digitalWriteFast(pin_size_t pin, PinStatus val) {
   PinName hardware_port_pin = g_APinDescription[pin].name;
   //uint16_t mask = 1 << (hardware_port_pin & 0xf);
   uint16_t mask = mask_table[hardware_port_pin & 0xf];
-  GPIO_TypeDef  * const port = port_table[hardware_port_pin >> 8];
+  GPIO_TypeDef  * const port = port_table[hardware_port_pin >> 4];
   if (val) port->BSRR = mask;
   else port->BSRR = (uint32_t)(mask << 16);
 }
@@ -53,7 +53,7 @@ static inline void digitalToggleFast(uint8_t pin) __attribute__((always_inline, 
 static inline void digitalToggleFast(pin_size_t pin) {
   PinName hardware_port_pin = g_APinDescription[pin].name;
   uint16_t pin_mask = mask_table[hardware_port_pin & 0xf];
-  GPIO_TypeDef  * const portX = port_table[hardware_port_pin >> 8];
+  GPIO_TypeDef  * const portX = port_table[hardware_port_pin >> 4];
 
   if (portX->ODR & pin_mask) portX->BSRR = (uint32_t)(pin_mask << 16);
   else portX->BSRR = pin_mask;
@@ -62,7 +62,7 @@ static inline void digitalToggleFast(pin_size_t pin) {
 static inline uint16_t digitalReadFast(pin_size_t pin) {
   PinName hardware_port_pin = g_APinDescription[pin].name;
   uint16_t pin_mask = mask_table[hardware_port_pin & 0xf];
-  GPIO_TypeDef  * const portX = port_table[hardware_port_pin >> 8];
+  GPIO_TypeDef  * const portX = port_table[hardware_port_pin >> 4];
 
   return (portX->IDR & pin_mask);
 }
